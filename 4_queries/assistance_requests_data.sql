4c33496d9b523ce19dde54cc1d07610f25c9a3ad
Fahad Kamal <fahadkamal@hotmail.com>
2023-10-01 07:19:24 -0400

    initial commit practice queries

---
+++ 4_queries/assistance_requests_data.sql
@@ -0,0 +1,7 @@
+SELECT teachers.name AS teacher, students.name AS student, assignments.name AS assignment, (assistance_requests.completed_at - assistance_requests.started_at) AS duration
+FROM teachers
+JOIN assistance_requests ON teachers.id = teacher_id
+JOIN students ON students.id = assistance_requests.student_id
+JOIN assignments ON assignments.id = assignment_id
+GROUP BY teacher, student, assignment, assistance_requests.completed_at, assistance_requests.started_at
+ORDER BY duration;
